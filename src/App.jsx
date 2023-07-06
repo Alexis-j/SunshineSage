e25f8e9fc826d647441e9d5d0818ce919120f027
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-07-06 11:52:13 +0200

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // console.log(import.meta.env.VITE_API_KEY)
 import { Box, Container, TextField, Typography, IconButton } from "@mui/material";
 import { useState } from "react";
+import { useRef } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import './App.css'; // Importa el archivo CSS
 
@@ -8,6 +9,7 @@ const API_WEATHER_CURRENT = `https://api.weatherapi.com/v1/current.json?key=${im
 const API_WEATHER_FORECAST = `https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_API_KEY}&q=`;
 
 export default function App() {
+  const inputRef = useRef(null);
   const [city, setCity] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState({ error: false, message: "" });
@@ -21,10 +23,10 @@ export default function App() {
     forecast: [],
   });
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setError({ error: false, message: "" });
+    const onSubmit = async () => {
+      event.preventDefault();
+      setLoading(true);
+      setError({ error: false, message: "" });
 
     try {
       if (!city.trim()) {
@@ -71,10 +73,10 @@ export default function App() {
 
 return (
   <div className="app-container" style={{ display: "flex", alignItems: "center", justifyContent: "center", minHeight: "100vh" }}>
-    <div style={{ ...styles, width: "95%", position: "absolute", }}>
+    <div style={{ ...styles, flex: "1", position: "relative", }}>
       <Container maxWidth="xs">
-        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
-          <Typography variant="h3" component="h1" align="center" gutterBottom>
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px", position: "relative", }}>
+          <Typography variant="h3" component="h2" align="center" gutterBottom>
             SunshineSage App
           </Typography>
           <Box
@@ -86,6 +88,13 @@ return (
             autoComplete="off"
             onSubmit={onSubmit}
           >
+          </Box>
+        </div>
+        </Container>
+          <Typography variant="h2" component="h1" style={{ color: "white", maxHeight:"50px" }}>
+              {weather.conditionText}
+          </Typography>
+          <div style={{ ...styles, width: "30%", position: "absolute",top:"-5.8%", right:"-3.6%", height: 100, backgroundColor:"#B6C0B8", borderRadius: "0 15px 15px 0"}}>
             <TextField
               id="City"
               label="City"
@@ -100,86 +109,87 @@ return (
               InputProps={{
                 endAdornment: (
                   <IconButton
-                    type="submit"
                     edge="end"
                     color="primary"
                     aria-label="Buscar"
                     onClick={onSubmit}
                   >
-                    <SearchIcon />
+                    <SearchIcon style={{ color: "#ffffff" }} />
                   </IconButton>
                 ),
+                onKeyDown: (e) => {
+                  if (e.key === "Enter") {
+                    e.preventDefault();
+                    onSubmit();
+                  }
+                }
               }}
+              inputRef={inputRef}
             />
-          </Box>
-        </div>
-      </Container>
-    </div>
-    <div style={{ ...styles, width: "30%", position: "absolute", right: 0, height: 100, backgroundColor:"#B6C0B8", borderRadius: "0 15px 15px 0"}}>
-      <Container maxWidth="xs">
-        {weather.city && (
-          <Box
-            sx={{
-              display: "grid",
-              gap: 2,
-              textAlign: "center",
-            }}
-          >
-            <Typography variant="h4" component="h2">
-              {weather.city}, {weather.country}
-            </Typography>
-            <Box
-              component="img"
-              alt={weather.conditionText}
-              src={weather.icon}
-              sx={{ margin: "0 auto" }}
-            />
-            <Typography variant="h5" component="h3">
-              {weather.temp}°C
-            </Typography>
-            <Typography variant="h6" component="h4">
-              {weather.conditionText}
-            </Typography>
-          </Box>
-        )}
-        {weather.forecast.length > 0 && (
-          <Box
-            sx={{
-              display: "grid",
-              gap: 2,
-              textAlign: "center",
-            }}
-          >
-            {weather.forecast.map((day) => (
-              <div key={day.date}>
-                <Typography variant="h6" component="h3">
-                  {day.date}
-                </Typography>
+            <Container maxWidth="xs">
+              {weather.city && (
                 <Box
-                  component="img"
-                  alt={day.day.condition.text}
-                  src={day.day.condition.icon}
-                  sx={{ margin: "0 auto" }}
-                />
-                <Typography variant="body1" component="p">
-                  {day.day.avgtemp_c}°C
-                </Typography>
-                <Typography variant="body2" component="p">
-                  {day.day.condition.text}
-                </Typography>
-              </div>
-            ))}
-          </Box>
-        )}
-        <Typography textAlign="center" sx={{ mt: 2, fontSize: "10px" }}>
-          Powered By:{" "}
-          <a href="http://www.weatherapi.com/" title="Weather API">
-            WeatherApi.com
-          </a>
-        </Typography>
-      </Container>
+                  sx={{
+                    display: "grid",
+                    gap: 2,
+                    textAlign: "center",
+                  }}
+                >
+                  <Typography variant="h4" component="h2">
+                    {weather.city}, {weather.country}
+                  </Typography>
+                  <Box
+                    component="img"
+                    alt={weather.conditionText}
+                    src={weather.icon}
+                    sx={{ margin: "0 auto" }}
+                  />
+                  <Typography variant="h5" component="h3">
+                    {weather.temp}°C
+                  </Typography>
+                  <Typography variant="h6" component="h4">
+                    {weather.conditionText}
+                  </Typography>
+                </Box>
+              )}
+              {weather.forecast.length > 0 && (
+                <Box
+                  sx={{
+                    display: "grid",
+                    gap: 2,
+                    textAlign: "center",
+                  }}
+                >
+                  {weather.forecast.map((day) => (
+                    <div key={day.date}>
+                      <Typography variant="h6" component="h3">
+                        {day.date}
+                      </Typography>
+                      <Box
+                        component="img"
+                        alt={day.day.condition.text}
+                        src={day.day.condition.icon}
+                        sx={{ margin: "0 auto" }}
+                      />
+                      <Typography variant="body1" component="p">
+                        {day.day.avgtemp_c}°C
+                      </Typography>
+                      <Typography variant="body2" component="p">
+                        {day.day.condition.text}
+                      </Typography>
+                    </div>
+                  ))}
+                </Box>
+              )}
+              <Typography textAlign="center" sx={{ mt: 2, fontSize: "10px", position: "absolute", top:"90%", left:"30%", }}>
+                Powered By:{" "}
+                <a href="http://www.weatherapi.com/" title="Weather API">
+                  WeatherApi.com
+                </a>
+              </Typography>
+            </Container>
+          </div>
+      </div>
     </div>
-  </div>
-);
-
+  );
 }
